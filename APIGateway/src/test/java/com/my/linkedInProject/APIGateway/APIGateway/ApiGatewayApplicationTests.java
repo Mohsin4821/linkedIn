29f5068ab3fdec95cf48a0e4035647289917047d
ShaikMdMohsin <shaik.mohsin@incture.com>
2025-06-09 15:51:10 +0530

    added api-gateway and discovery client

---
+++ APIGateway/src/test/java/com/my/linkedInProject/APIGateway/APIGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.linkedInProject.APIGateway.APIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ ConnectionsService/src/test/java/com/my/linkedInProject/ConnectionsService/ConnectionsService/ConnectionsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.linkedInProject.ConnectionsService.ConnectionsService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectionsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
